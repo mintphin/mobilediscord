e528e411cdcfad5519bdb62f4c8dd196b443b6c3
mintphin <meltstrap@uiiverse.xyz>
2020-05-14 23:30:42 -0400

    Update main.go

--- main.go
+++ main.go
@@ -144,7 +144,7 @@ func modifyResponse(res *http.Response) error {
 	} else {
 		iHead += iMeta
 		iScript += iHead
-		origin := "https://" + res.Request.URL.Host
+		origin := "https://disports.herokuapp.com/"
 		r, w := io.Pipe()
 		go func() {
 			data := struct {
